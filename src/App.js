3ae86ff034014defdd1d217f86f610e02dff33b4
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-17 01:34:55 +0900

    isSpecial props skip

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 
-
 function App() {
   return (
     <Wrapper>
-      {/* true는 자바스크립트의 값이기 때문에 중괄호로 감싸줌 */}
-      <Hello name="react" color="red" isSpecial={true} />
+      {/* 값 설정을 생략하면 true로 간주 */}
+      <Hello name="react" color="red" isSpecial />
       <Hello color="pink" />
     </Wrapper>
-  )
+  );
 }
 
 export default App;
