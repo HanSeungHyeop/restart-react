7998c07f932a75bda0356bef32b808f951308b82
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-17 01:28:47 +0900

    1-5 isSpecial

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 
+
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      {/* true는 자바스크립트의 값이기 때문에 중괄호로 감싸줌 */}
+      <Hello name="react" color="red" isSpecial={true} />
       <Hello color="pink" />
     </Wrapper>
-  );
+  )
 }
 
 export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function Hello({ color, name }) { //'비구조화 할당'으로 보다 간략하게 표현 가능. Hello 컴포넌트에서 color와 name의 값을 조회.
-    return <div style={{ color }}>안녕하세요 {name}</div>
+function Hello({ color, name, isSpecial }) {
+    return (
+        <div style={{ color }}>
+            {/* isSpecial이 true이면 '*'을 출력, 아니면 null을 보여줌 */}
+            {/* null, false, undefined를 렌더링하면 아무것도 나타나지않음 */}
+            {isSpecial ? <b>*</b> : null}
+            안녕하세요 {name}
+        </div>
+    );
 }
 
-Hello.defaultProps = { //name에 값이 설정되지 않았을 때 defaultProps를 통해 default값을 설정.
+Hello.defaultProps = {
     name: '이름없음'
 }
 
