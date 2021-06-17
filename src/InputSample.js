8b2a1b8529cb15ffb9752813ee48187462fd303f
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-17 15:54:25 +0900

    1-9

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,26 +1,43 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-    // useState의 기본값을 ''으로 설정
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        // name과 nickname의 초기값을 각각 ''로 설정
+        name: '',
+        nickname: ''
+    });
+
+    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
-        // e.target.value를 통해 이벤트 객체에 담겨있는 텍스트를 setText로 불러옴
-        setText(e.target.value);
+        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+        setInputs({
+            ...inputs, // 기존의 inputs 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
+        });
     };
-    const onReset = (e) => {
-        // onReset이 실행될 때 setText의 값은 ''로 바뀜
-        setText('');
+
+    const onReset = () => {
+        setInputs({
+            // onReset이 실행되면 setInputs함수에 의해 name과 nickname의 값이 각각 ''로 바뀜
+            name: '',
+            nickname: '',
+        })
     };
+
+
     return (
         <div>
-            {/* onChange함수를 통해 input박스의 값을 text로 전달함 */}
-            <input onChange={onChange} value={text} />
-            {/* onClick함수로 '초기화'버튼을 클릭시 onRest함수가 실행됨 */}
+            {/* 첫번째 input박스의 키값을 name으로 설정함. onChange함수로 전달된 이벤트 값 'name'을 표시함. */}
+            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            {/* 두번째 input박스의 키값을 nickname으로 설정함. onChange함수로 전달된 이벤트 값 'nickname'을 표시함. */}
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
+            {/* onClick함수로 초기화 버튼을 클릭시 onRest함수가 실행돼 name과 nickname의 값이 각각 ''로 바뀜 */}
             <button onClick={onReset}>초기화</button>
             <div>
-                {/* input박스에서 전달된 text값을 표시함 */}
-                <b>값: {text}</b>
+                <b>값: </b>
+                {/* setInputs로 전달받은 name과 nickname 값을 각각 표시 */}
+                {name} ({nickname})
             </div>
         </div>
     );
